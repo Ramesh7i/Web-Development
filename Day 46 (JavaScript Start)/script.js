8851b46fb2ae96b1c76914f803e15ccf53e7a65c
Ramesh7i <ramesh6280ic@gmail.com>
2026-09-10 01:47:43 +0530

    Start Javascript

---
+++ Day 46 (JavaScript Start)/script.js
@@ -0,0 +1,11 @@
+// alert("Sunny what are you doing");
+
+console.log("Code is runing...")
+console.log("Code is runing like a bot...")
+
+// var a = prompt("Enter your number")
+// var isTrue = confirm("Blast your computer")
+
+document.title = "Hey sunny"
+
+document.body.style.backgroundColor = "pink"
