b245646b236f494366430dd4b18456a41dd53178
Ramesh7i <ramesh6280ic@gmail.com>
2026-09-11 00:25:14 +0530

    JavaScript Variables, Data Types & Objects

---
+++ Day 47 ( JavaScript Variables)/script.js
@@ -0,0 +1,35 @@
+console.log("Hey its me Sunny")
+
+let a = 6;
+let b = 5;
+let c = "Sid";
+
+console.log(a + b)
+
+console.log(typeof a, typeof b, typeof c)
+
+// const a1 = 6;
+// // a1 = a1+1;
+
+let x = "Sun";
+let y = 21;
+let z = 3.56;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y, typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Sidharth",
+    "age": 20,
+    "arive": true
+}
+console.log(o)
+
+o.salaery = "100 cr"
+console.log(o)
+
+o.salaery = "500 cr"
+console.log(o)
