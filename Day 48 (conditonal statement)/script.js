294c87c731397cd180a0ffa823d629d68ab5ff12
Ramesh7i <ramesh6280ic@gmail.com>
2026-09-11 00:54:39 +0530

    JavaScript Conditionals: if, else if, else ladder

---
+++ Day 48 (conditonal statement)/script.js
@@ -0,0 +1,32 @@
+console.log("hii what should do ")
+
+let age = 0;
+let grace = 2;
+
+// age += grace
+
+console.log(age)
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+if(age>18){
+    console.log("You can drive")
+}
+
+else if(age == 0){
+    console.log("Are you kidding?")
+}
+else{
+    console.log("You can not")
+}
+
+q = 5;
+r = 6;
+let c= q>r ?(q-r):(r-q);
+
+console.log(c)
