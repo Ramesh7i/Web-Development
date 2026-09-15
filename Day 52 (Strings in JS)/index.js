95582348548ce585354346b704126a23ce2059be
Ramesh7i <ramesh6280ic@gmail.com>
2026-09-16 00:26:36 +0530

    Strings

---
+++ Day 52 (Strings in JS)/index.js
@@ -0,0 +1,27 @@
+let a = "Sunny";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length)
+
+let myname = "Sidhath"
+let friend = "Ramesh"
+
+console.log("his name is "+myname+" His friend name is "+ friend)
+
+console.log(`his name is ${myname} His friend name is ${friend}`)
+
+let b = "Rohan"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+
+console.log(b.slice(1,3))
+console.log(b.slice(1))
+
+console.log(b.replace("Ro", "67"))
+console.log(b.concat(a," Sallu bhai", "Priya"))
+
